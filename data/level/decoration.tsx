09baefac18eba12527bc1f6c1e9ba64ed75ddee9
gth-other <cheb0tarev.nikit0s@gmail.com>
2022-10-30 23:39:17 +0300

    Небольшие изменения, чтобы редактор карт не ругался.

--- data/level/decoration.tsx
+++ data/level/decoration.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="decoration" tilewidth="32" tileheight="32" tilecount="160" columns="16">
- <image source="../images/tiles/decoration.png" width="512" height="320"/>
+ <image source="../images/tiles/decoration.png" width="640" height="320"/>
 </tileset>

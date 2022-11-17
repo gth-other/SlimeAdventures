f25fe093f059dc57cf2dc1c818504425a9ddf8f6
gth-other <cheb0tarev.nikit0s@gmail.com>
2022-11-17 16:49:51 +0300

    Исправлена ошибка.

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ void endgame(SDL_Renderer *renderer, Camera camera) {
 int main() {
     LinkManager::set_directory("..", "..");
 
-    std::filesystem::create_directory(LinkManager::path("", true));
+    std::filesystem::create_directory(LinkManager::path("", false));
     if (!std::filesystem::is_directory(LinkManager::path("appdata", false))) std::filesystem::copy(LinkManager::path("data/appdata", true), LinkManager::path("appdata", false));
 
     SDL_Init(SDL_INIT_EVERYTHING);
